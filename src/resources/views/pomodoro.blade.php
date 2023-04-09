9080a249d08d88016b66683f23a574bb52a2e9bc
Smith11764 <fuji.feather10@gmail.com>
2023-04-09 11:23:39 +0900

    feat: 🎸 サイドバーを実装した

--- src/resources/views/pomodoro.blade.php
+++ src/resources/views/pomodoro.blade.php
@@ -16,10 +16,10 @@
     <script src="{{ asset('js/todo.js') }}"></script>
 </head>
 <body>
-    <!-- Auth Buttons Container -->
-    <div class="auth-buttons-container">
+    <!-- sidebar -->
+    <div class="sidebar">
         @auth
-            <a class="nes-btn is-primary" href="{{ route('logout') }}"
+            <a href="{{ route('logout') }}"
                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 Logout
             </a>
@@ -28,91 +28,100 @@
             </form>
         @endauth
         @guest
-            <a class="nes-btn is-primary" href="{{ route('login') }}">Login</a>
-            <a class="nes-btn is-success" href="{{ route('register') }}">Register</a>
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
         @endguest
-    </div>
-    <!-- about_section -->
-    <div class="nes-container is-dark">
-        <div class="header-container">
-            <img class="rapper_image" src="{{ asset('images/clock.png') }}" alt="rapper image">
-            <h1>Pomodoro Beats</h1>
-            <img class="rapper_image" src="{{ asset('images/clock.png') }}" alt="rapper image">
-        </div>
-        <!-- Balloon -->
-        <h2>How to use this?</h2>
-        <p>
-            Click "START!!!" button.<br>
-            You can choose your background music at "Music" section.<br>
-            Also, you can add your task at "ToDo" section.<br>
-            When you finish your task, click "Done" button.<br>
-        </p>
+        <a href="{{ route('pomodoro') }}">Home</a>
+        <a href="#music_section">Music</a>
+        <a href="#todo_section">ToDo</a>
+        <!-- 他のリンクを追加する場合は、ここに追加してください。 -->
     </div>
 
-    <!-- timer_section -->
-    <div class="nes-container is-dark timer-section-container">
-        <div class="timer-container">
-            <p id="timer"></p>
-            <div>
-                <i id="pomodoro_count">count:0</i>
-                <i class="nes-icon is-small star is-empty"></i>
-                <i class="nes-icon is-small star is-empty"></i>
-                <i class="nes-icon is-small star is-empty"></i>
-                <i class="nes-icon is-small star is-empty"></i>
+    <!-- main -->
+    <!-- Auth Buttons Container -->
+    <div class="main-content">
+        <!-- about_section -->
+        <div class="nes-container is-dark">
+            <div class="header-container">
+                <img class="rapper_image" src="{{ asset('images/clock.png') }}" alt="rapper image">
+                <h1>Pomodoro Beats</h1>
+                <img class="rapper_image" src="{{ asset('images/clock.png') }}" alt="rapper image">
             </div>
-            <progress class="nes-progress is-primary" id="progress_bar" value="100" max="100"></progress>
-            <div>
-                <button class="nes-btn" id="start-pause">START!!!</button>
-                <button class="nes-btn" id="reset">RESET</button>
-            </div>
+            <!-- Balloon -->
+            <h2>How to use this?</h2>
+            <p>
+                Click "START!!!" button.<br>
+                You can choose your background music at "Music" section.<br>
+                Also, you can add your task at "ToDo" section.<br>
+                When you finish your task, click "Done" button.<br>
+            </p>
         </div>
-        <div class="instructor-container">
-            <div>
-                <div class="nes-balloon from-left is-dark">
-                    <p id="rapper_comment">What's up?</p>
+
+        <!-- timer_section -->
+        <div class="nes-container is-dark timer-section-container">
+            <div class="timer-container">
+                <p id="timer"></p>
+                <div>
+                    <i id="pomodoro_count">count:0</i>
+                    <i class="nes-icon is-small star is-empty"></i>
+                    <i class="nes-icon is-small star is-empty"></i>
+                    <i class="nes-icon is-small star is-empty"></i>
+                    <i class="nes-icon is-small star is-empty"></i>
                 </div>
-                <img class="rapper_image" src="{{ asset('images/rapper.png') }}" alt="rapper image">
-            </div>
-        </div>
-    </div>
-
-    <!-- music_section -->
-    <div class="nes-container is-dark music-container with-title">
-        <p class="title">Music</p>
-{{--        <p>Choose your music.</p>--}}
-        <p>coming soon..</p>
-
-{{--        <div class="music-src-pomodoro-beats">--}}
-{{--            <button class="nes-btn">Play from Pomodoro Beats</button>--}}
-{{--        </div>--}}
-{{--        <div class="music-src-spofity music-container">--}}
-{{--            <button class="nes-btn">Play Spotify</button>--}}
-{{--            <img class="rapper_image" src="{{ asset('images/Spotify_Icon_RGB_Green.png') }}" alt="spofity icon">--}}
-{{--        </div>--}}
-{{--        <div class="music-src-soundcloud">--}}
-{{--            <button class="nes-btn">Play SoundCloud</button>--}}
-{{--        </div>--}}
-    </div>
-
-    <!-- todo_section -->
-    <div id="todo_section" class="nes-container is-dark music-container with-title" data-delete-image="{{ asset('images/batsu.png') }}" data-up-image="{{ asset('images/up_arrow.png') }}" data-down-image="{{ asset('images/down_arrow.png') }}">
-        <p class="title">ToDo</p>
-        <div class="input-container">
-            <label for="textarea_field">write your task</label>
-            <textarea id="textarea_field" class="nes-textarea"></textarea>
-            <button id="add_task_button" class="nes-btn">Add Task</button>
-        </div>
-
-        <!-- task_review_container -->
-        <div class="task_review_container">
-            <div class="task_review_section">
-                <div class="nes-container is-dark music-container with-title" id="todo_task">
-                    <p class="title">ToDo</p>
+                <progress class="nes-progress is-primary" id="progress_bar" value="100" max="100"></progress>
+                <div>
+                    <button class="nes-btn" id="start-pause">START!!!</button>
+                    <button class="nes-btn" id="reset">RESET</button>
                 </div>
             </div>
-            <div class="task_review_section">
-                <div class="nes-container is-dark music-container with-title" id="done_task">
-                    <p class="title">Done</p>
+            <div class="instructor-container">
+                <div>
+                    <div class="nes-balloon from-left is-dark">
+                        <p id="rapper_comment">What's up?</p>
+                    </div>
+                    <img class="rapper_image" src="{{ asset('images/rapper.png') }}" alt="rapper image">
+                </div>
+            </div>
+        </div>
+
+        <!-- music_section -->
+        <div class="nes-container is-dark music-container with-title">
+            <p class="title">Music</p>
+    {{--        <p>Choose your music.</p>--}}
+            <p>coming soon..</p>
+
+    {{--        <div class="music-src-pomodoro-beats">--}}
+    {{--            <button class="nes-btn">Play from Pomodoro Beats</button>--}}
+    {{--        </div>--}}
+    {{--        <div class="music-src-spofity music-container">--}}
+    {{--            <button class="nes-btn">Play Spotify</button>--}}
+    {{--            <img class="rapper_image" src="{{ asset('images/Spotify_Icon_RGB_Green.png') }}" alt="spofity icon">--}}
+    {{--        </div>--}}
+    {{--        <div class="music-src-soundcloud">--}}
+    {{--            <button class="nes-btn">Play SoundCloud</button>--}}
+    {{--        </div>--}}
+        </div>
+
+        <!-- todo_section -->
+        <div id="todo_section" class="nes-container is-dark music-container with-title" data-delete-image="{{ asset('images/batsu.png') }}" data-up-image="{{ asset('images/up_arrow.png') }}" data-down-image="{{ asset('images/down_arrow.png') }}">
+            <p class="title">ToDo</p>
+            <div class="input-container">
+                <label for="textarea_field">write your task</label>
+                <textarea id="textarea_field" class="nes-textarea"></textarea>
+                <button id="add_task_button" class="nes-btn">Add Task</button>
+            </div>
+
+            <!-- task_review_container -->
+            <div class="task_review_container">
+                <div class="task_review_section">
+                    <div class="nes-container is-dark music-container with-title" id="todo_task">
+                        <p class="title">ToDo</p>
+                    </div>
+                </div>
+                <div class="task_review_section">
+                    <div class="nes-container is-dark music-container with-title" id="done_task">
+                        <p class="title">Done</p>
+                    </div>
                 </div>
             </div>
         </div>
