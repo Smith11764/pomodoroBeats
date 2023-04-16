57bdeb7692f2664d010931089eb19493be160a97
Smith11764 <fuji.feather10@gmail.com>
2023-04-16 19:15:21 +0900

    feat: 🎸 ログイン画面のデザインを変更した

--- src/resources/views/components/auth-card.blade.php
+++ src/resources/views/components/auth-card.blade.php
@@ -1,9 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div style="width: 45%;margin: 10% auto;">
     <div>
-        {{ $logo }}
-    </div>
+{{--        {{ $logo }}--}}
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+        <div>
+            <div>
+                {{ $slot }}
+            </div>
+        </div>
     </div>
 </div>
